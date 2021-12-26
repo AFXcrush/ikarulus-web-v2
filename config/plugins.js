a660e1cc4bf21dc560f4bb3add277f01ce788d88
Afx <afxcrush@gmail.com>
2021-12-26 15:29:17 -0500

    db fixed

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,16 @@
-// module.exports = ({ env }) => ({
-//   upload: {
-//     provider: "cloudinary",
-//     providerOptions: {
-//       cloud_name: env("CLOUDINARY_NAME"),
-//       api_key: env("CLOUDINARY_KEY"),
-//       api_secret: env("CLOUDINARY_SECRET"),
-//     },
-//     actionOptions: {
-//       upload: {
-//         folder: "ikarus-web",
-//       },
-//       delete: {},
-//     },
-//   },
-// });
+module.exports = ({ env }) => ({
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
+    },
+    actionOptions: {
+      upload: {
+        folder: "ikarus-web",
+      },
+      delete: {},
+    },
+  },
+});
