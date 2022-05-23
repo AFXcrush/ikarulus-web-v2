beef149e668ae3d74565a95f0c2d33562720427a
Afx <afxcrush@gmail.com>
2022-05-22 22:43:52 -0500

    Strapi Vercel added

--- config/server.js
+++ config/server.js
@@ -1,9 +1,26 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b670117dc9923e04de07782c2d3ae238'),
+      secret: env("ADMIN_JWT_SECRET", "b670117dc9923e04de07782c2d3ae238"),
+    },
+  },
+  vercel: {
+    // Required
+    token: env("VERCEL_TOKEN"),
+    // Required
+    projectId: env("VERCEL_PROJECT_ID"),
+    // Optional (required if you use teams)
+    teamId: env("VERCEL_TEAM_ID"),
+    // Required (hooks)
+    triggers: {
+      production: env("VERCEL_TRIGGER_PRODUCTION"),
+    },
+    // Optional
+    config: {
+      // Number of latest deployments
+      deployments: 10,
     },
   },
 });
